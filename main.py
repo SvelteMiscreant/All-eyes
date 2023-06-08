36b3028c1e0560e88ed4bb04736bb98643ee68f2
Miscr34nt <112925859+SvelteMiscreant@users.noreply.github.com>
2023-06-08 18:59:23 +0530

    Update main.py
    Made it update in real time and added a virtual kali linux as dependency.

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-import struct
 import socket
-import binascii
+import struct
 
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.htons(0x0800))
-while True:
-  packet = s.recvfrom(2048)
+def packet_sniffer():
+    sniffer = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
-ethernet_header = packet[0][0:14]
-eth_header = struct.unpack("!6s6s2s", ethernet_header)
+    while True:
+        raw_data, addr = sniffer.recvfrom(65536)
 
-print("Destination MAC:" + binascii.hexlify(eth_header[0]) + " Source MAC:" + binascii.hexlify(eth_header[1]) + " Type:" + binascii.hexlify(eth_header[2]))
+        eth_length = 14
+        eth_header = raw_data[:eth_length]
+        eth = struct.unpack('! 6s 6s H', eth_header)
+        src_mac = get_mac_address(eth[1])
+        dest_mac = get_mac_address(eth[0])
+
+        print("Source MAC: {src_mac}")
+        print("Destination MAC: {dest_mac}")
+
+        ip_header = raw_data[eth_length:eth_length + 20]
+        iph = struct.unpack('!BBHHHBBH4s4s', ip_header)
+        version_ihl = iph[0]
+        ihl = version_ihl & 0xF
+        iph_length = ihl * 4
+        src_ip = socket.inet_ntoa(iph[8])
+        dest_ip = socket.inet_ntoa(iph[9])
+
+        print("Source IP: {src_ip}")
+        print("Destination IP: {dest_ip}")
+
+        print("-------------------")
+
+def get_mac_address(mac_bytes):
+    mac_string = map('{:02x}'.format, mac_bytes)
+    return ':'.join(mac_string)
+
+packet_sniffer()
