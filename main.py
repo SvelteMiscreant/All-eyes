9b0d1a209c032bd40ee56258a2798ae57427b574
Miscr34nt <112925859+SvelteMiscreant@users.noreply.github.com>
2023-06-10 08:02:30 +0530

    Update main.py
    Added protocol names

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def packet_sniffer():
         src_mac = get_mac_address(eth[1])
         dest_mac = get_mac_address(eth[0])
 
-        print("Source MAC: " + src_mac)
-        print("Destination MAC: " + dest_mac)
+        print("Source MAC: ", src_mac)
+        print("Destination MAC: ", dest_mac)
 
         ip_header = raw_data[eth_length:eth_length + 20]
         iph = struct.unpack('!BBHHHBBH4s4s', ip_header) # Extracted IP header in correct tuple format
@@ -24,10 +24,12 @@ def packet_sniffer():
         src_ip = socket.inet_ntoa(iph[8]) # Making the source IP Address human readable
         dest_ip = socket.inet_ntoa(iph[9])
         protocol = iph[6] # Protocol extracted from IP Header tuple
+        protocol_string = socket.getservbyport(protocol) # Protocol name, change line as it's buggy
 
-        print("Source IP: " + src_ip)
-        print("Destination IP: " + dest_ip)
-        print("Protocol: " + str(protocol))
+        print("Source IP: ", src_ip)
+        print("Destination IP: ", dest_ip)
+        #print("Protocol Value: ", str(protocol))
+        print("Protocol: ", protocol_string)
 
         print("-------------------")
 
