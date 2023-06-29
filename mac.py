6cff81af5e14dfef22a6e5d96da9600119faf23a
SvelteMiscreant <am4ns1ngh5@gmail.com>
2023-06-29 13:27:29 -0400

    Added MAC manipulation

---
+++ mac.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def allow_mac_address(mac_address):
+	command = ['sudo', 'iptables', '-A', 'INPUT', '-m', 'mac', '--mac-source', mac_address, '-j', 'ACCEPT']
+	subprocess.run(command)
+
+def block_mac_address(mac_address):
+	command = ['sudo', 'iptables', '-A', 'INPUT', '-m', 'mac', '--mac-source', mac_address, '-j', 'DROP']
+	subprocess.run(command)
+
+def mac():
+	mac_address = input('Enter the MAC address to allow/block: ')
+	action = str.lower(input("Enter 'allow' to allow or 'block' to block: "))
+	
+	if mac_address:
+		if action == 'allow':
+		    allow_mac_address(mac_address)
+		    print('The MAC address ', mac_address, ' has been allowed.')
+		elif action == 'block':
+		    block_mac_address(mac_address)
+		    print('The MAC address ', mac_address, ' has been blocked.')
+		else:
+		    print("Invalid action. Please enter 'allow' or 'block'.")
--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import sys
 from packetsniffer import packet_sniffer
 import port as p
 import ip
+import mac
 
-print('1. Block port')
-print('2. Open port')
-print('3. Block IP address')
-print('4. Allow IP address')
+print('1. Block Port')
+print('2. Open Port')
+print('3. Block IP Address')
+print('4. Allow IP Address')
+print('5. MAC Address Control')
 
 option = input('Choose your action: ')
 
@@ -21,6 +23,8 @@ elif option == '3':
 	ip.block_ip()
 elif option == '4':
 	ip.allow_ip()
+elif option == '5':
+	mac.mac()
 
 sniff = str.upper(input('Would you like to start reading packets? Y or N\n'))
 
